69459eaee43ec6db9d87e09b67625328cb62e9e2
fasseg <frank.asseg@congrace.de>
2012-06-13 13:32:37 +0200

    Added triggering mechanism for Jenkins ======================================  - this script now checks the files in /var/run/sync-git/ for the SHA-1 hashes of the repositories' heads triggers a Jenkins build if the SHA1 has changes in comparison to the stored value.  - fixed the error function to exit after an error occured and include a small message anout the nature of the rerror

--- sync-git.sh
+++ sync-git.sh
@@ -9,14 +9,38 @@
 GIT_REPOSITORIES=/data/git-repositories
 LOG_PATH=/var/log/git-sync.log
 CURRENT_PATH=`pwd`
+RUN_DIRECTORY=/var/run/sync-git
+JENKINS_URL=http://whvmescidoc5.fiz-karlsruhe.de:8484
 
+# use an associative array for storing the maps between git and jenkins project names
+# WARNING: don't use underscores '_' in array names, that got me in trouble!
+declare -A projects 
+
+projects[escidoc-browser]="eSciDocBrowser"
+projects[escidoc-github]="eSciDoc-Core"
+projects[escidoc-metadata-updater]="escidoc-metadata-updater"
 
 function error {
 	# send an email to admin and log to /var/log/daemon.*
+    # then exit so nothing can get crushed
 	logger -p daemon.err An error occured while syncing the git repositories.
-	/bin/echo "An error has occured while syncing the git repositories. Please check the log at ${LOG_PATH} at host ${HOSTNAME}. This is a generated message." | /usr/bin/mail -s "SYNC Error" "eSciDocAdmin@fiz-karlsruhe.de" -- -r "git-sync-cronjob@whvmescidoc4.fiz-kalrsruhe.de"
+	echo -e "An error has occured while syncing the git repositories. Please check the log at ${LOG_PATH} at host ${HOSTNAME}. This is a generated message.\n\nMessage text:\n$1\n" | /usr/bin/mail -s "SYNC Error" "eSciDocAdmin@fiz-karlsruhe.de" -- -r "git-sync-cronjob@whvmescidoc4.fiz-karlsruhe.de"
+    exit -1
 }
 
+function trigger_jenkins {
+    # trigger the build on the jenkins server
+    PROJECT=`expr match $1 '\([a-zA-Z\-]*\)'`
+    if [ -z ${projects[$PROJECT]} ]; then
+        echo "skipping build of unknown project $PROJECT" &>> $LOG_PATH
+        return
+    fi
+    echo "triggering build of $PROJECT" >> $LOG_PATH
+    curl -sL -w "Jenkins returned: %{http_code} for  %{url_effective}\\n" "$JENKINS_URL/job/${projects[$PROJECT]}/build?delay=0sec" -o /dev/null &>> $LOG_PATH
+	RETVAL=$?
+	[ $RETVAL -eq 0 ] && logger -p daemon.info triggered build of $PROJECT succesfully
+	[ $RETVAL -ne 0 ] && error "unable to trigger build on $JENKINS_URL for $PROJECT" 
+}
 
 # check if a valid user is running this script and ask if he knows what he's doing
 if [ "$(id -u)" == 0 ]; then
@@ -48,21 +72,22 @@ for FOLDER in $GIT_REPOSITORIES/* ; do
         echo "skipping non git repository $FOLDER"
         continue
     fi
-
+    
+    REPO_NAME=`basename $FOLDER`
 	REMOTE_URI=`git remote -v | grep -E "^origin.*\(fetch\)$" | awk '{print $2;}'` 
 
     echo ":: syncing git repository $REMOTE_URI" &>> $LOG_PATH
 	git fetch origin &>> $LOG_PATH
 	RETVAL=$?
 	[ $RETVAL -eq 0 ] && logger -p daemon.info fetched successfully from $REMOTE_URI
-	[ $RETVAL -ne 0 ] && error
+	[ $RETVAL -ne 0 ] && error "unable to fetch from $REMOTE_URI"
 	
 	# and dont forget to prune old branches from the local repository
 	# that have already been deleted at origin.
 	git remote prune origin &>> $LOG_PATH
 	RETVAL=$?
 	[ $RETVAL -eq 0 ] && logger -p daemon.info pruned succesfully from $REMOTE_URI
-	[ $RETVAL -ne 0 ] && error
+	[ $RETVAL -ne 0 ] && error "unable to prune branches from $REMOTE_URI"
 
     #iterate over all the tracking branches on origin and update the corresponding refs
     for BRANCH in `git branch -r | grep -E "^\s*origin\/" | sed "s/^\s*origin\///"` ; do
@@ -70,8 +95,22 @@ for FOLDER in $GIT_REPOSITORIES/* ; do
         git update-ref refs/heads/$BRANCH refs/remotes/origin/$BRANCH &>> $LOG_PATH
 	    RETVAL=$?
     	[ $RETVAL -eq 0 ] && logger -p daemon.info updated branch $BRANCH succesfully from $REMOTE_URI
-	    [ $RETVAL -ne 0 ] && error
+	    [ $RETVAL -ne 0 ] && error "unable to update refs for $BRANCH from $REMOTE_URI"
     done
+
+    #check if the HEAD has changed in order to be able to trigger a Jenkins rebuild
+    if [ ! -e $RUN_DIRECTORY/$REPO_NAME.head ]; then
+        # first time run, so we better trigger a rebuild
+        trigger_jenkins $REPO_NAME 
+    elif [ `cat $RUN_DIRECTORY/$REPO_NAME.head` != `git rev-parse HEAD` ]; then
+        trigger_jenkins $REPO_NAME
+    fi
+
+    #write the SHA1 hash of the HEADS to RUN_DIRECTORY so that they can be compared 
+    #during the next run to establish wether the "master" has changed in order to trigger
+    #jenkins rebuilds
+    echo `git rev-parse HEAD` > $RUN_DIRECTORY/$REPO_NAME.head
+
 done
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
